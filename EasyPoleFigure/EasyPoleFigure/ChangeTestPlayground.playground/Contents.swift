6855b1cbb5c4162f5f01fc360e1fb12c5422e200
csw8820 <84608524+csw8820@users.noreply.github.com>
2022-04-06 12:04:58 +0900

    여러가지
    여러가지 시도

--- EasyPoleFigure/EasyPoleFigure/ChangeTestPlayground.playground/Contents.swift
+++ EasyPoleFigure/EasyPoleFigure/ChangeTestPlayground.playground/Contents.swift
@@ -1,6 +1,39 @@
 import UIKit
 import Matft
 
+
+var str =  "1 0 0"
+
+
+
+var arr = str.components(separatedBy: " ")
+
+print(arr)
+
+
+//func projection(_ pole: [Double]) -> [Double] {
+//    let p_prime = [pole[0]/(1-pole[2]), pole[0]/(1-pole[2])]
+//
+//    return p_prime
+//
+//}
+//projection([sqrt(3)/3, sqrt(3)/3, sqrt(3)/3])
+//
+//
+//             func makeM() -> [Double] {
+//
+//}
+
+let circlePath = UIBezierPath(arcCenter: CGPoint(x: 100, y: 100), radius: CGFloat(20), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true) //CGPoint(x,y)의 위치가 원의 중심입니다.
+let shapeLayer = CAShapeLayer()
+shapeLayer.path = circlePath.cgPath //change the fill color
+shapeLayer.fillColor = UIColor.clear.cgColor //you can change the stroke color
+shapeLayer.strokeColor = UIColor.red.cgColor //you can change the line width
+shapeLayer.lineWidth = 3.0
+view.layer.addSublayer(shapeLayer)
+
+
+
 //print(pow(0.1, 5.0))
 
 let s = MfArray([[0.0, 0.1, 0.2],[1.0, 1.1, 1.2]])
--- EasyPoleFigure/EasyPoleFigure/ViewController.swift
+++ EasyPoleFigure/EasyPoleFigure/ViewController.swift
@@ -7,10 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+func projection(_ pole: [Double]) -> [Double] {
+    let p_prime = [pole[0]/(1-pole[2]), pole[1]/(1-pole[2])]
+    
+    return p_prime
+    
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var userInput = textField.text!.components(separatedBy: " ")
+        
+        let p_prime = projection([Double(userInput[0]), Double(userInput[1]), Double(userInput[2])])
+        
+        let x_0 = view.center.x
+        let y_0 = view.center.y
+        
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: x_0, y: y_0), radius: CGFloat(100), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true) //CGPoint(x,y)의 위치가 원의 중심입니다.
+        let shapeLayer1 = CAShapeLayer()
+        shapeLayer1.path = circlePath.cgPath
+        shapeLayer1.fillColor = UIColor.clear.cgColor //change the fill color
+        shapeLayer1.strokeColor = UIColor.red.cgColor //you can change the stroke color
+        shapeLayer1.lineWidth = 1.0 //you can change the line width
+        view.layer.addSublayer(shapeLayer1)
+        
+        let dotPath = UIBezierPath(arcCenter: CGPoint(x: x_0+100*p_prime[0], y: y_0+100*p_prime[1]), radius: CGFloat(1), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        let shapeLayer2 = CAShapeLayer()
+        shapeLayer2.path = dotPath.cgPath
+        shapeLayer2.fillColor = UIColor.clear.cgColor //change the fill color
+        shapeLayer2.strokeColor = UIColor.blue.cgColor //you can change the stroke color
+        shapeLayer2.lineWidth = 2.0 //you can change the line width
+        view.layer.addSublayer(shapeLayer2)
+        
         // Do any additional setup after loading the view.
     }
 
