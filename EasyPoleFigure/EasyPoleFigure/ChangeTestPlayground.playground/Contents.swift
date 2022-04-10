52581221f8704159c42e6621b80b792142ab601b
csw8820 <84608524+csw8820@users.noreply.github.com>
2022-04-10 22:19:23 +0900

    Update Contents.swift
    func __60_120_rot111__,
    func __mirror_110__수정
    
    func __rot_90_180_270__수정중 -3x3x3행렬에서 문제

--- EasyPoleFigure/EasyPoleFigure/ChangeTestPlayground.playground/Contents.swift
+++ EasyPoleFigure/EasyPoleFigure/ChangeTestPlayground.playground/Contents.swift
@@ -19,8 +19,96 @@ let zero3x3x3Matrix: [simd_double3x3] = [simd_double3x3(), simd_double3x3(), sim
 print("zero3x3x3Matrix = ",zero3x3x3Matrix)
 
 // MARK: 3x3 단위행렬 만들기 = np.identity(3)
+/*
+ 1 0 0
+ 0 1 0
+ 0 0 1
+ */
 let one3X3Matrix = simd_double3x3(1)
 print("one3X3Matrix = ",one3X3Matrix)
+
+simd_double3x3()
+
+//// symmetry operators
+//// h는 넣어줄 행렬
+//func __60_120_rot111__(h :[simd_double3x3]) -> [simd_double3x3] {
+//    /*
+//    For the given h operation,
+//    rotations of (pi/3) & (2*pi/3) around <111>
+//    are performed and returned
+//    *cubic
+//    */
+////    hx = h.copy()
+//    let hx = h
+////    let h60 = np.zeros((3,3)) // 값이 0인 3*3 행렬
+//    var h60 = simd_double3x3(0)
+////    let h120 = np.zeros((3,3))
+//    var h120 = simd_double3x3(0)
+//    h60[0, 2] = 1.0
+//    h60[1, 0] = 1.0
+//    h60[2, 1] = 1.0
+//
+//    h120[0, 1] = 1.0
+//    h120[1, 2] = 1.0
+//    h120[2, 0] = 1.0
+//
+////    return np.dot(h60, hx), np.dot(h120, hx)
+//    return simd_dot(h60, hx), simd_dot(h120, hx)
+//}
+
+//func __mirror_110__(h :[simd_double3x3]) -> [simd_double3x3] {
+//    /*
+//    Given the operation h, mirrored across the (110) plane returned
+//    *cubic
+//    */
+////    hx = h.copy()
+//    let hx = h
+////    hm = np.zeros((3,3))
+//    var hm = simd_double3x3(0)
+//    hm[0, 1] = 1.0
+//    hm[1, 0] = 1.0
+//    hm[2, 2] = 1.0
+//
+//    let result = simd_dot(hm, hx)
+//    return result
+//}
+//func __rot_90_180_270__(h :[simd_double3x3]) -> [simd_double3x3] {
+//    /*
+//    Given the operation h,
+//    the three rotated operations are returned
+//    *cubic
+//    */
+////    cos = np.cos; sin = np.sin; pi = np.pi
+////    let cos = simd.cos(<#T##Double#>)
+////    let sin = Matft.math.sin
+//    let pi = Double.pi
+////    hx = np.zeros((3,3,3))
+//    let hx = [simd_double3x3(0), simd_double3x3(0), simd_double3x3(0)]
+////    h_ = h.copy(); htemp = []
+//    let h_ = h
+//    let htemp = [Double]()
+//    for m in 0...3 {
+////        ang = pi/2. * float(m+1)
+//        let ang = pi / 2.0 * Double(m+1)
+//        hx[m, 0, 0] = simd.cos(ang)
+//        hx[m, 1, 1] = cos(ang)
+//        hx[m, 2, 2] = 1.0
+//        hx[m, 0, 1] = -sin(ang)
+//        hx[m, 1, 0] = sin(ang)
+//        hx[m, 0, 2] = 0.0
+//        hx[m, 2, 0] = 0.0
+//        hx[m, 1, 2] = 0.0
+//        hx[m, 2, 1] = 0.0
+//        //pass
+//    }
+//    for m in 0...3 {
+//        //htemp.append( np.dot(hx[m], h_) )
+//        htemp.append(simd_dot(hx[m], h))
+//        //pass
+//    }
+////    return np.array(htemp)
+//    return MfArray(htemp)
+//}
 
 // MARK: 행렬 회전, 라디안값으로 넣어줘야함
 /* 아래 코드와 동일
@@ -48,16 +136,17 @@ func makeRotationMatrix(angle: Double) -> simd_double3x3 {
 
 let h_n: simd_double3x3 = simd_double3x3(rows: [[-1,1,7],[1,-1,-21],[2,1,-1]])
 let tempDot = simd_double3x3(rows: [[-1,3,2],[2,-1,4],[3,2,-1]])
-print(h_n * tempDot)
+print("h_n * tempDot = ",h_n * tempDot)
 
 
-//print(simd_dot(simd_double2([2,1]), simd_double2([3,7])))
+print("simd_dot = ", simd_dot(simd_double2([2,1]), simd_double2([3,7])))
 
 
 
-
+//// stereographic projection 계산식
+//// [X, Y] = [x/(1-z), y/(1-z)]
 //func projection(_ pole: [Double]) -> [Double] {
-//    let p_prime = [pole[0]/(1-pole[2]), pole[0]/(1-pole[2])]
+//    let p_prime = [pole[0]/(1-pole[2]), pole[1]/(1-pole[2])]
 //
 //    return p_prime
 //
