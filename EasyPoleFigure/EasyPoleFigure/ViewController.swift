a0b09d412a67d1610f01161b95db07c251144f69
csw8820 <84608524+csw8820@users.noreply.github.com>
2022-04-15 00:59:46 +0900

    Update ViewController.swift
    함수명, 변수명 변경
    x_0, y_0 변경(서브뷰에서 중심을 잡아야하는데 잘 안되는듯)

--- EasyPoleFigure/EasyPoleFigure/ViewController.swift
+++ EasyPoleFigure/EasyPoleFigure/ViewController.swift
@@ -17,12 +17,12 @@ func projection(_ pole: [Double]) -> [Double] {
 class ViewController: UIViewController {
     
     @IBOutlet weak var millerX: UITextField!
-    
     @IBOutlet weak var millerY: UITextField!
-    
     @IBOutlet weak var millerZ: UITextField!
     
     @IBOutlet weak var editButton: UIButton!
+    
+    @IBOutlet weak var resultView: UIView!
     
     var x_0: CGFloat = 0
     var y_0: CGFloat = 0
@@ -30,58 +30,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        x_0 = view.center.x
-        y_0 = view.center.y
+        x_0 = resultView.bounds.midX
+        y_0 = resultView.bounds.midY // 좀 더 서브뷰에서 중앙에 가까운듯
+//        x_0 = resultView.center.x
+//        y_0 = resultView.center.y
         
-        drawCircle()
+        drawMainCircle()
         // Do any additional setup after loading the view.
     }
 
-    fileprivate func drawCircle() {
+    fileprivate func drawMainCircle() {
         
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: x_0, y: y_0), radius: CGFloat(100), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true) //CGPoint(x,y)의 위치가 원의 중심입니다.
-        let shapeLayer1 = CAShapeLayer()
-        shapeLayer1.path = circlePath.cgPath
-        shapeLayer1.fillColor = UIColor.clear.cgColor //change the fill color
-        shapeLayer1.strokeColor = UIColor.red.cgColor //you can change the stroke color
-        shapeLayer1.lineWidth = 1.0 //you can change the line width
-        view.layer.addSublayer(shapeLayer1)
+        let mainCirclePath = UIBezierPath(arcCenter: CGPoint(x: x_0, y: y_0), radius: CGFloat(100), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true) //CGPoint(x,y)의 위치가 원의 중심입니다.
+        let shapeLayerMainCircle = CAShapeLayer()
+        
+        shapeLayerMainCircle.path = mainCirclePath.cgPath
+        shapeLayerMainCircle.fillColor = UIColor.clear.cgColor //change the fill color
+        shapeLayerMainCircle.strokeColor = UIColor.red.cgColor //you can change the stroke color
+        shapeLayerMainCircle.lineWidth = 1.0 //you can change the line width
+        resultView.layer.addSublayer(shapeLayerMainCircle)
     }
     
-    fileprivate func drawPoint(_ p_prime: [Double]) {
-        let dotPath = UIBezierPath(arcCenter: CGPoint(x: x_0+100*p_prime[0], y: y_0+100*p_prime[1]), radius: CGFloat(1), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
-        let shapeLayer2 = CAShapeLayer()
-        shapeLayer2.path = dotPath.cgPath
-        shapeLayer2.fillColor = UIColor.clear.cgColor //change the fill color
-        shapeLayer2.strokeColor = UIColor.blue.cgColor //you can change the stroke color
-        shapeLayer2.lineWidth = 2.0 //you can change the line width
-        view.layer.addSublayer(shapeLayer2)
+    fileprivate func drawPoleFigure(_ p_prime: [Double]) {
+        let poleFigure = UIBezierPath(arcCenter: CGPoint(x: x_0+100*p_prime[0], y: y_0+100*p_prime[1]), radius: CGFloat(1), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        let shapeLayerPoleFigure = CAShapeLayer()
+        
+        shapeLayerPoleFigure.path = poleFigure.cgPath
+        shapeLayerPoleFigure.fillColor = UIColor.clear.cgColor //change the fill color
+        shapeLayerPoleFigure.strokeColor = UIColor.blue.cgColor //you can change the stroke color
+        shapeLayerPoleFigure.lineWidth = 2.0 //you can change the line width
+        resultView.layer.addSublayer(shapeLayerPoleFigure)
         
     }
+    
     @IBAction func editButtonClicked(_ sender: UIButton) {
         let userInputX = millerX.text!
         let userInputY = millerY.text!
         let userInputZ = millerZ.text!
         let p_prime = projection([Double(userInputX) ?? 0, Double(userInputY) ?? 0, Double(userInputZ) ?? 0])
-        drawPoint(p_prime)
+        drawPoleFigure(p_prime)
     }
-    
 }
-
-
-//extension ViewController: UITextFieldDelegate {
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        let userInputX = millerX.text!
-//        let userInputY = millerY.text!
-//        let userInputZ = millerZ.text!
-//        let p_prime = projection([Double(userInputX) ?? 0, Double(userInputY) ?? 0, Double(userInputZ) ?? 0])
-//        drawPoint(p_prime)
-//    }
-//    // 이게 버튼을 눌렀을 때 실행되도록 바꿔야함
-//
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//        return true
-//    }
-//
-//}
