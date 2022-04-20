abf827ae1351bd61f47f3f56db11761f6963413e
csw8820 <84608524+csw8820@users.noreply.github.com>
2022-04-20 11:40:09 +0900

    수업중 업데이트
    rotation 행렬 추가필요

--- EasyPoleFigure/EasyPoleFigure/ViewController.swift
+++ EasyPoleFigure/EasyPoleFigure/ViewController.swift
@@ -26,12 +26,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        x_0 = resultView.bounds.size.width / 2
-//        y_0 = resultView.bounds.size.height / 2
-        x_0 = resultView.bounds.midX
-        y_0 = resultView.bounds.midY // 좀 더 서브뷰에서 중앙에 가까운듯
+        x_0 = resultView.bounds.size.width / 2
+        y_0 = resultView.bounds.size.height / 2
+//        x_0 = resultView.bounds.midX
+//        y_0 = resultView.bounds.midY // 좀 더 서브뷰에서 중앙에 가까운듯
 //        x_0 = resultView.center.x
 //        y_0 = resultView.center.y
+        print(x_0, y_0)
+        print(resultView.bounds.midX, resultView.bounds.midY)
+        print(resultView.center.x, resultView.center.y)
+        
         drawMainCircle()
     }
 
@@ -65,18 +69,21 @@ class ViewController: UIViewController {
         let userInputX = millerX.text!
         let userInputY = millerY.text!
         let userInputZ = millerZ.text!
-        let p = SIMD3<Double>(simd_normalize(simd_double3(x: Double(userInputX) ?? 0, y: Double(userInputY) ?? 0, z: Double(userInputZ) ?? 0)))
+        let p = SIMD3<Double>(simd_normalize(simd_double3(x: Double(userInputX) ?? 0, y: Double(userInputY) ?? 0, z: Double(userInputZ) ?? 0))) // 사용자 입력값을 정규화하여 저장(x, y, z)
         var pDotH: [SIMD3<Double>] = []
         var p_prime: [SIMD2<Double>] = []
         
         for i in cubicData.indices {
             pDotH.append(dotP(cubicData[i], p))
-        }
+        } // 정규화된 값과 cubic symmetry와 내적 [(x', y', z')]
+        
+        // 회전을 시켰을 때 Euler Angle 이용하여 [XZX] dot [(x', y', z')] = [(x'', y'', z'')]
+        
         for i in pDotH.indices {
             p_prime.append(projection(pDotH[i]))
-        }
+        } // 내적된 값을 projection하여 저장 [(X, Y)]
         for i in p_prime.indices {
             drawPoleFigure(p_prime[i])
-        }
+        } // [(X, Y)] 값들을 좌표계에 그림
     }
 }
