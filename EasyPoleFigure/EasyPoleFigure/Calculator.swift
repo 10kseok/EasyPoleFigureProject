082b55190d18fa4f07bc78daf25f586a66c166ff
csw8820 <84608524+csw8820@users.noreply.github.com>
2022-05-02 03:15:31 +0900

    Update Calculator.swift

--- EasyPoleFigure/EasyPoleFigure/Calculator.swift
+++ EasyPoleFigure/EasyPoleFigure/Calculator.swift
@@ -18,6 +18,13 @@ func dotP(_ symmetryOperator: double3x3,_ milerIdx: SIMD3<Double>) -> SIMD3<Doub
     return SIMD3<Double>(x: x, y: y, z: z)
 }
 
+//func rotatingDotP(_ a: SIMD3<Double>,_ milerIdx: SIMD3<Double>) -> SIMD3<Double> {
+//    let x = dot(symmetryOperator[0], milerIdx)
+//    let y = dot(symmetryOperator[1], milerIdx)
+//    let z = dot(symmetryOperator[2], milerIdx)
+//    return SIMD3<Double>(x: x, y: y, z: z)
+//}
+
 
 func projection(_ pole: SIMD3<Double>) -> SIMD2<Double> {
     let p_prime = SIMD2([pole[0]/(1-pole[2]), pole[1]/(1-pole[2])])
@@ -26,14 +33,15 @@ func projection(_ pole: SIMD3<Double>) -> SIMD2<Double> {
 }
 
 
-func calcEulerAngle(_ angleX: Double, _ angleY: Double, _ angleZ: Double, _ standardPole: [SIMD3<Double>]) -> [SIMD3<Double>] {
-    let radian = pi / 180.0
-    let c1 = cos(angleX * radian)
-    let c2 = cos(angleY * radian)
-    let c3 = cos(angleZ * radian)
-    let s1 = sin(angleX * radian)
-    let s2 = sin(angleY * radian)
-    let s3 = sin(angleZ * radian)
+//func calcEulerAngle(_ angleX: Double, _ angleY: Double, _ angleZ: Double, _ standardPole: [SIMD3<Double>]) -> [SIMD3<Double>] {
+func calcEulerAngle(_ angleX: Double, _ angleY: Double, _ angleZ: Double) -> [SIMD3<Double>] {
+
+    let c1 = cos(angleX)
+    let c2 = cos(angleY)
+    let c3 = cos(angleZ)
+    let s1 = sin(angleX)
+    let s2 = sin(angleY)
+    let s3 = sin(angleZ)
     
     let _0_0 = c2*c3
     let _0_1 = -c2*s3
